14b3ad0f8ea2ab5bafc0ca37e74870b5c74a682c
koya-ido <koya.development0@gmail.com>
2024-11-14 21:27:09 +0900

    add nuxt3

---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="wrapper">
+    <h1>gitの練習</h1>
+    <AppButton btnTitle="次へ" @click="onClickBtn" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const onClickBtn = () => {
+  navigateTo("/practice");
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
